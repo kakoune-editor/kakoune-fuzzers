6da7fac691d9cf46e976bebcbf38e9901f44cb58
Frank LENORMAND <lenormf@gmail.com>
2020-10-22 12:05:56 +0300

    regex: Use idiomatic data provider API
    LibFuzzer provides with a helper class that allows extracting data
    out of the random input, which allows the fuzzer's results to be
    reproducible.

---
+++ regex/search.cc
@@ -0,0 +1,29 @@
+#include <fuzzer/FuzzedDataProvider.h>
+
+#include <string.hh>
+#include <regex.hh>
+#include <exception.hh>
+
+using namespace Kakoune;
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    FuzzedDataProvider fuzzed_data(data, size);
+
+    const size_t regex_length = fuzzed_data.ConsumeIntegral<uint8_t>();
+    const std::string regex_string = fuzzed_data.ConsumeBytesAsString(regex_length);
+    const std::string text_string = fuzzed_data.ConsumeRemainingBytesAsString();
+
+    try
+    {
+        StringView text = {text_string.c_str(), text_string.length()};
+        MatchResults<String::const_iterator> matches;
+        Regex regex = Regex({regex_string.c_str(), regex_string.length()});
+        regex_search(text.begin(), text.end(),
+                     text.begin(), text.end(),
+                     matches, regex);
+    }
+    catch (runtime_error&) {}
+
+    return 0;
+}
